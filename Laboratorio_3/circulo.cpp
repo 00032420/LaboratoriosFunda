e64fac6162a02eb423093b46371abc1a9d8d93bd
CR_Avila <00032420@uca.edu.sv>
2020-05-07 10:49:16 -0600

    fix pres

--- Laboratorio_3/circulo.cpp
+++ Laboratorio_3/circulo.cpp
@@ -1,8 +1,9 @@
-//Se incluyen librerías para la validación de datos, conversión de string a numeros y realización de operaciónes matemáticas
 #include <iostream>
-#include <string>
-#include <cstdlib>
-#include <cmath>
+#include <string> //Para string a float
+#include <cstdlib> 
+#include <cmath> // para Pi y pow()
+#include <iomanip> //Para std__setpresicion
+
 
 using namespace std;
 
@@ -11,7 +12,7 @@ float convertir(string numero){ //Se pone como argumento el string de la entrada
     //Si la entrada contiene algun simbolo no admitido o fuera de rango, se hace lo siguiente:
     while(cin >> numero && numero.find_first_not_of("1234567890.") != string::npos){
         //Se muestra la advertencia
-        cout << "Numero invalido" << endl;
+        cout << endl << "Numero invalido" << endl;
         if(atof(numero.c_str()) < 0){ //si el numero ingresado es negativo, también se advierte
             cout << "El radio de un circulo no puede ser negativo." << endl;
         }
@@ -22,28 +23,36 @@ float convertir(string numero){ //Se pone como argumento el string de la entrada
         //Se descarta la entrada anterior
         cin.ignore(123, '\n');
     }
-
+    cout << endl;
     //Si se introdujo un numero valido, se devuelve convertido a float. 
     return atof(numero.c_str());
 }
 
 //Se define el area en funcion del radio
-double area(double radio){
+float area(double radio){
     return M_PI * (pow(radio,2));
 }
 
 //Se define el perimetro en funcion del radio
-double perimetro(double radio){
+float perimetro(double radio){
     return 2 * M_PI * radio;
 }
 
+//Se define la aproximacion a dos decimales
+float aprox(double num){
+    float valor = (int)(num*100 + 0.5 );
+    return (float) valor/100;
+}
+
 int main(){
-    //Se define la variable tipo string donde se introducirá el numero
-    string radio;
+    //Se define la variable tipo string donde se introducirá el numero, y 
+    string radio, rep;
     //Se definen las variables en donde se almacenara e radio y los calculos
-    double r, p, a;
+    float r, p, a;
+    bool repetir; //Valor de repetición, si es verdadero el programa se reinicia para hacer las operaciones de nuevo.
     
-    //Se solicita la entrada del radio
+    //Se solicita la entrada del radio 
+    do{
     cout << "Ingrese el radio del circulo: ";
     //Se verifica y convierte el radio, y a la vez se guarda en la variable numerica
     r = convertir(radio);
@@ -51,11 +60,23 @@ int main(){
     //Se evaluan las funciones en con respecto al radio, y se guardan en sus respectivas variables
     p = perimetro(r);
     a = area(r);
+    
+    //Se muestran los resultados, en fixed para evitar notacion cientifica y presicion de dos decimales
+    cout << "El perimetro del circulo es " << std::fixed << std::setprecision(2) << p << endl;
+    cout << "El area del circulo es " << std::fixed << std::setprecision(2) << a << endl;
+    cout << endl;
+    cout << "Desea repetir la operacion? [S/N] ";
 
-    //Se muestran los resultados
-    cout << "El perimetro del circulo es " << p << endl;
-    cout << "El area del circulo es " << a << endl;
+    cin >> rep;
 
-    system("pause");
+    if(rep == "s" || rep == "S"){
+        repetir = true;
+        cout << endl;
+    } else {
+        repetir = false;
+    }
+
+    } while(repetir == true);
+
     return 0;
 }
