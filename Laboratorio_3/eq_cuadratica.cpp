9b821b71ed9334671825e95916006ec6f577c935
CR_Avila <00032420@uca.edu.sv>
2020-05-09 16:07:49 -0600

    mejorar estructura

--- Laboratorio_3/eq_cuadratica.cpp
+++ Laboratorio_3/eq_cuadratica.cpp
@@ -25,35 +25,53 @@ float leer(string numero){ //Se pone como argumento el string de la entrada
     return atof(numero.c_str());
 }
 
+bool reiniciar(bool repetir){
+    cout << "Desea repetir la operacion? [S/N] ";
+    string rep;
+        cin >> rep;
 
-
-float rmas(float a, float b, float c, float dis){
-    return ((-b)+sqrt(dis))/(2*a);
+            if(rep == "s" || rep == "S"){
+            //Si se introduce una s, mayuscula o minuscula, el valor repeteición queda verdadero y se repite el ciclo
+            cout << endl;
+            return true;
+            }
+            //si no se introduce una s, el valor repeticion queda falso y se termina el programa. 
+            return false;         
 }
 
-float rmenos(float a, float b, float c, float dis){
-    return ((-b)-sqrt(dis))/(2*a);
+
+
+void d_positivo(float a, float b, float c, float dis){
+    float r1 = ((-b)+sqrt(dis))/(2*a);
+    float r2 = ((-b)-sqrt(dis))/(2*a);
+    cout << "X1: " << std::fixed << std::setprecision(2) << r1 << endl;
+    cout << "X2: " << std::fixed << std::setprecision(2) << r2 << endl;
 }
 
-float rcero(float a, float b){
-    return (-b)/(2*a);
+void d_cero(float a, float b){
+    float x = (-b)/(2*a);
+    cout << "X: " << std::fixed << std::setprecision(2) << x << endl;
 }
 
-void r_comp(float a, float b, float c, float dis){
+void d_negativo(float a, float b, float c, float dis){
     float parte_real = ( (-b)/(2*a));
     float parte_img = ( sqrt( -dis)/(2*a));
+
+    cout << "La ecuacion no tiene solucion en los numeros reales." << endl;
+    cout << "Pero si en los complejos:" << endl << endl;
+
     
-    cout << "X1: " << parte_real << " + " << parte_img <<  "i" << endl;;
-    cout << "X2: " << parte_real << " - " << parte_img <<  "i" << endl;;
+    cout << "X1: " << parte_real << " + " << std::fixed << std::setprecision(2) << parte_img <<  "i" << endl;;
+    cout << "X2: " << parte_real << " - " << std::fixed << std::setprecision(2) << parte_img <<  "i" << endl;;
 }
 
 
 int main(){
     //Se define la variable tipo string donde se introducirá el numero, y otro en caso de no tener solución 
-    string x2, x1, x0,rep, img1, img2;
+    string x2, x1, x0;
     
     //Se definen las variables en donde se almacenara e radio y los calculos
-    float a, b, c, d, r1, r2;
+    float a, b, c, d;
     bool repetir; //Valor de repetición, si es verdadero el programa se reinicia para hacer las operaciones de nuevo.
     
     do{
@@ -72,36 +90,18 @@ int main(){
         d = (b*b)-4*a*c;
 
         if(d >0){
-            r1 = rmas(a,b,c,d);
-            r2 = rmenos(a,b,c,d);
-            cout << "X1: " << std::fixed << std::setprecision(2) << r1 << endl;
-            cout << "X2: " << std::fixed << std::setprecision(2) << r2 << endl;
+            d_positivo(a,b,c,d);
         } else if( d == 0){
-            r1 = rcero(a,b);
-            cout << "X: " << std::fixed << std::setprecision(2) << r1 << endl;
-        } else
-        {
-            
-            cout << "La ecuacion no tiene solucion en los numeros reales." << endl;
-            cout << "Pero si en los complejos :v" << endl << endl;
-            r_comp(a,b,c,d);
+            d_cero(a,b);  
+        } else {  
+            d_negativo(a,b,c,d);
         }
         
         cout << endl;
-        cout << "Desea repetir la operacion? [S/N] ";
 
         //se pregunta si se quiere relizar la operación de nuevo
-        cin >> rep;
-
-            if(rep == "s" || rep == "S"){
-            //Si se introduce una s, mayuscula o minuscula, el valor repeteición queda verdadero y se repite el ciclo
-            repetir = true;
-            cout << endl;
-            } else {
-            //si no se introduce una s, el valor repeticion queda falso y se termina el programa. 
-            repetir = false;
-            }
-
+        repetir = reiniciar(repetir);
+        
     } while(repetir == true);
 
     return 0;
