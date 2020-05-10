4be59dddc85d586d2c09c8db6ca31b435acb6c86
CR_Avila <00032420@uca.edu.sv>
2020-05-10 14:15:30 -0600

    se reestructuran las funciones radio y perimetro

--- Laboratorio_3/circulo.cpp
+++ Laboratorio_3/circulo.cpp
@@ -28,6 +28,7 @@ float leer(string numero){ //Se pone como argumento el string de la entrada
     return atof(numero.c_str());
 }
 
+//Funcion para reiniciar las operaciones
 bool reiniciar(bool repetir){
     cout << "Desea repetir la operacion? [S/N] ";
     //Se crea la variable para almacenar la respuesta del usuario
@@ -39,21 +40,24 @@ bool reiniciar(bool repetir){
     return rep == "s" || rep == "S" ? true : false;
 }
 
-//Se define el area en funcion del radio
-float area(double radio){
-    return M_PI * (pow(radio,2));
+void area(double radio){
+    //Se define el area en funcion del radio
+    float a = M_PI * (pow(radio,2));
+    //Se muestra el resultado, en modo fixed y dos decimales.
+    cout << "El area del circulo es " << std::fixed << std::setprecision(2) << a << endl;  
 }
 
 //Se define el perimetro en funcion del radio
-float perimetro(double radio){
-    return 2 * M_PI * radio;
+void perimetro(double radio){
+    float p = 2 * M_PI * radio;
+    cout << "El perimetro del circulo es " << std::fixed << std::setprecision(2) << p << endl;   
 }
 
 int main(){
     //Se define la variable tipo string donde se introducirá el numero, y 
     string radio;
     //Se definen las variables en donde se almacenara e radio y los calculos
-    float r, p, a;
+    float r;
     //Valor de repetición, si es verdadero el programa se reinicia para hacer las operaciones de nuevo.
     bool repetir; 
     
@@ -64,14 +68,10 @@ int main(){
         r = leer(radio);
 
         //Se evaluan las funciones en con respecto al radio, y se guardan en sus respectivas variables
-        p = perimetro(r);
-        a = area(r);
-    
-        //Se muestran los resultados, en fixed para evitar notacion cientifica y presicion de dos decimales
-        cout << "El perimetro del circulo es " << std::fixed << std::setprecision(2) << p << endl;
-        cout << "El area del circulo es " << std::fixed << std::setprecision(2) << a << endl;
+        perimetro(r);
+        area(r); 
         cout << endl;
-        
+
         //Se pregunta si se quiere repetir la operacion 
         repetir = reiniciar(repetir);
 
